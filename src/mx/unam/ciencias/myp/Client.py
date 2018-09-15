0afb89fd0893cee6827be3363fa3619db2aebeb6
Teresa Becerril <terebece1508@ciencias.unam.mx>
2018-09-15 18:56:56 -0500

    Estructura de clases

---
+++ src/mx/unam/ciencias/myp/Client.py
@@ -0,0 +1,23 @@
+import socket
+import threading
+import sys
+
+class Client():
+
+    def __init__(self, clientSock):
+
+    def setAddress(self):
+
+    def getAddress(self):
+
+    def setSocket(self):
+
+    def getSocket(self):
+
+    def creatThreading(self):
+
+    def msgRecv(self):
+
+    def msgSend(self, msg):
+
+    def clearLine(self):
---
+++ src/mx/unam/ciencias/myp/Server.py
@@ -0,0 +1,25 @@
+import socket
+import sys
+import threading
+
+class TestServer(unittest.TestCase):
+
+    def __init__(self, serverSock, clients):
+
+    def creatSocket(self):
+
+    def creatThreading(self):
+
+    def getAddress(self):
+
+    def setAddress(self):
+
+    def setSocket(self):
+
+    def getSocket(self):
+
+    def msgAll(self):
+
+    def acceptConn(self):
+
+    def processConn(self):
