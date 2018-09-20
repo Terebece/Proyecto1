2697bbd840ede17d5d6b5ad7de18c44f3b2d15b8
Teresa Becerril <terebece1508@ciencias.unam.mx>
2018-09-20 16:35:47 -0500

    MainServer y MainClient seran los controladores para la interfaz grafica

--- src/mx/unam/ciencias/myp/MainClient.py
+++ src/mx/unam/ciencias/myp/MainClient.py
@@ -1,11 +1,7 @@
 import Client
 
 def main():
-    host = str(input("Enter host: "))
-    port = int(input("Enter port: "))
 
-    client = Client.Client(host, port)
-    client.runClient()
 
 if __name__ == '__main__':
     main()
