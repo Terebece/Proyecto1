4544bc58f8229c38286644a3d4c6b8584b18e960
Teresa Becerril <terebece1508@ciencias.unam.mx>
2018-09-21 00:31:31 -0500

    Documentacion de Room y User

--- src/mx/unam/ciencias/myp/Room.py
+++ src/mx/unam/ciencias/myp/Room.py
@@ -1,33 +1,59 @@
 class Room():
 
     def __init__(self, roomname, creator):
+        """Constructor que inicializa una sala con: su nombre, el cliente que lo
+        creo, una lista de los clientes que fueron invitados y una lista de los
+        clientes que aceptaron la invitacion a la sala."""
         self.roomname = roomname
         self.creator = creator
         self.guestUser = []
         self.userInRoom = []
 
     def getRoomName(self):
+        """
+        Metodo que regresa el nombre de la sala.
+        """
         return self.roomname
 
     def setRoomName(self, roomname):
+        """
+        Metodo que cambia el nombre de la sala.
+        """
         self.roomname = roomname
 
     def getGuestUser(self):
+        """
+        Metodo que regresa la lista de los clientes invitados a la sala.
+        """
         return self.guestUser
 
     def getUsersInRoom(self):
+        """
+        Metodo que regresa la lista de los clientes en la sala.
+        """
         return self.userInRoom
 
     def addGuestUsers(self, creator, inv):
+        """
+        Metodo que agrega a los clientes invitados por el creador en la lista
+        de invitados.
+        """
         if creator == self.creator:
             self.guestUser.extend(inv)
         else:
             print("No tienes permiso para invitar personas a esta sala.")
 
     def addUsersInRoom(self, user):
+        """
+        Metodo que agrega a los clientes que aceptaron la invitacion de unirse
+        a la sala.
+        """
         self.userInRoom.append(user)
 
     def msgInRoom(self, client, msg):
+        """
+        Metodo para enviar mensajes en la sala.
+        """
         for user in self.userInRoom:
             if user != client:
                 user.getConn().append(msg.encode("utf8"))
--- src/mx/unam/ciencias/myp/User.py
+++ src/mx/unam/ciencias/myp/User.py
@@ -4,6 +4,11 @@ class User():
     salas = []
 
     def __init__(self, connection, address):
+        """
+        Constructor que inicializa a un usuario con: su conexion, su direccion,
+        su estado, una bandera para saber si el usuario ya se identifico y pueda
+        utilizar el chat y el nombre del usuario.
+        """
         self.connection = connection
         self.address = address
         self.status = "ACTIVE"
@@ -11,31 +16,58 @@ class User():
         self.name = " "
 
     def setConn(self, connection):
+        """
+        Metodo que define una nueva conexion para el usuario.
+        """
         self.connection = connection
 
     def getConn(self):
+        """
+        Metodo que regresa la conexion del usuario.
+        """
         return self.connection
 
     def setAddress(self, address):
+        """
+        Metodo que define una nueva direccion para el usuario.
+        """
         self.address = address
 
     def getAddress(self):
+        """
+        Metodo que regresa la direccion del usuarioself.
+        """
         return self.address
 
     def getName(self):
+        """
+        Metodo que regresa el nombre del Usuario.
+        """
         return self.name
 
     def setName(self, name):
+        """
+        Metodo que define un nuevo nombre para el usuario.
+        """
         self.name = name
         self.identified = True
 
     def getIdentified(self):
+        """
+        Metodo que nos regresa si el usuario ya se identifico.
+        """
         return self.identified
 
     def getStatus(self):
+        """
+        Metodo que regresa el estado del usuario.
+        """
         return self.userstatus
 
     def setStatus(self, status):
+        """
+        Metodo que define un nuevo estado para el usuario.
+        """
         for s in self.userstatus:
             if s == status:
                 self.status = status
